47c44ba7c466a585f59df21fe66c9afef55f4eb2
WakeeAnjum <162007177+WakeeAnjum@users.noreply.github.com>
2024-03-03 12:53:29 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,19 @@
+console.log("hello world");
+console.log("wakeel anjum");
+let message="wakeel anjum"
+console.log("message");
+// variables are like Container 
+// var
+// let
+// const
+let message1="hi eric would you like to learn python from me"
+console.log(message1);
+
+// string
+// number
+// boolean
+
+let message2="wakeel anjum"
+let message3=2024
+let message4=true
+console.log(message2,message3,message4);
