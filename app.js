15a2d573e9fa9908dfd0af2d445c451cd67efb2b
WakeeAnjum <162007177+WakeeAnjum@users.noreply.github.com>
2024-03-13 18:06:00 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 "use strict";
-console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new.”');
+let famousPerson = "famous person";
+let message = "Albert Einstein once said, “A person who never made a mistake never tried anything new.”";
+console.log(famousPerson, message);
