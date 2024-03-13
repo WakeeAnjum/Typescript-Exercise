d54fe912b3fe1e9a6f0e5bd495bf0518ed20b88d
WakeeAnjum <162007177+WakeeAnjum@users.noreply.github.com>
2024-03-13 18:06:49 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
-let famousPerson="famous person";
-let message="Albert Einstein once said, “A person who never made a mistake never tried anything new.”";
-console.log(famousPerson,message);
+let whitespace="\n\twakeel anjum\t\n";
+console.log(whitespace);
+let whithoutwhitespace=whitespace.trim();
+console.log(whithoutwhitespace);
