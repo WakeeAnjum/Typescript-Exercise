ae8c3983956e9ba7447ce718faf81d72f416ab72
WakeeAnjum <162007177+WakeeAnjum@users.noreply.github.com>
2024-03-13 18:03:26 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,19 +1,2 @@
-console.log("hello world");
-console.log("wakeel anjum");
-let message="wakeel anjum"
-console.log("message");
-// variables are like Container 
-// var
-// let
-// const
-let message1="hi eric would you like to learn python from me"
-console.log(message1);
-
-// string
-// number
-// boolean
-
-let message2="wakeel anjum"
-let message3=2024
-let message4=true
-console.log(message2,message3,message4);
+let personName="wakeel anjum"
+console.log('hello',personName,"would you like to learn some python today");
