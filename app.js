60fff264442c3ba2127a590c0a146dc91f2aaaa4
WakeeAnjum <162007177+WakeeAnjum@users.noreply.github.com>
2024-03-14 22:22:45 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 "use strict";
-let favoriteNumber = 11;
-let message = "My Favorite Number is";
-console.log(message, favoriteNumber);
+// wakeel anjum
+// date: 11-03-2024
+// let userName="wakeel anjum";
+// let message="hi wakeel would you like to learn python today";
+// console.log(userName,message);
